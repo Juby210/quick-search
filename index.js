f08fdaee25153b4f88e9524c2f2076a01e81c081
Juby210 <31005896+Juby210@users.noreply.github.com>
2020-05-29 22:43:44 +0200

    powercord api changed

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ const Settings = require('./Settings')
 
 module.exports = class TextReact extends Plugin {
     startPlugin() {
-        this.registerSettings('quick-search', 'Quick Search', Settings)
+        powercord.api.settings.registerSettings('quick-search', {
+            category: this.entityID,
+            label: 'Quick Search',
+            render: Settings
+        })
 
-        this.registerCommand('search', [],
-            'Quick search things in ddg.',
-            '{c} <query>',
-            async args => {
-                if(!args[0]) {
-                    return { result: 'The search query cannot be empty.' }
-                }
+        powercord.api.commands.registerCommand({
+            aliases: [],
+            command: 'search',
+            description: 'Quick search things in ddg.',
+            usage: '{c} <query>',
+            executor: async args => {
+                if (!args[0]) return { result: 'The search query cannot be empty.' }
 
                 const query = encodeURIComponent(args.join(' ')).replace(/%20/g, '+')
                 const req = await get('https://duckduckgo.com/html').query('q', query)
-                if(req.statusCode != 200) return { result: 'Something went wrong :(' }
+                if (req.statusCode != 200) return { result: 'Something went wrong :(' }
                 
                 let result = { type: 'rich', title: 'Search results', description: '', color: 0xe98622 }
                 const res = new DOMParser().parseFromString(req.body.toString(), 'text/html')
 
-                if(this.settings.get('showInstantAnswear') && res.querySelector('.zci')) {
+                if (this.settings.get('showInstantAnswear') && res.querySelector('.zci')) {
                     let title = res.querySelector('.zci__heading'), url = title.querySelector('a').getAttribute('href')
                     let zres = res.querySelector('.zci__result'), r = zres.textContent.trim().split('\n')
                     r[1] = `[[${r.pop().trim()}]](${url})`
@@ -32,19 +36,24 @@ module.exports = class TextReact extends Plugin {
                 }
                 
                 res.querySelectorAll('.result>.result__body').forEach((r, i) => {
-                    if(i >= this.settings.get('maxResults', 3)) return
-                    if(result.description != '') result.description += '\n'
+                    if (i >= this.settings.get('maxResults', 3)) return
+                    if (result.description != '') result.description += '\n'
                     const url = decodeURIComponent(r.querySelector('.result__url').getAttribute('href').replace('/l/?kh=-1&uddg=', ''))
                     result.description += `**[${r.querySelector('.result__a').textContent}](${url})**`
                     result.description += `\n${r.querySelector('.result__snippet').textContent}\n`
                 })
 
-                if(this.settings.get('showSearchInBrowser')) {
+                if (this.settings.get('showSearchInBrowser')) {
                     result.description += `\n[[Google]](https://www.google.com/search?q=${query}) | [[DuckDuckGo]](https://duckduckgo.com/?q=${query})`
                 }
 
                 return { result }
             }
-        )
+        })
+    }
+
+    pluginWillUnload() {
+        powercord.api.settings.unregisterSettings('quick-search')
+        powercord.api.commands.unregisterCommand('search')
     }
 }
